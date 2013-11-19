a2325dd0695636ce84d850e6c7ee71625e42b903
Hector Ramos <hector.ramos@ninthdesign.com>
2013-11-19 21:06:29 +0800

    modified style.css and rotator.js

--- js/rotator.js
+++ js/rotator.js
@@ -1,15 +1,25 @@
-/* */
-var loginName = prompt("Who are you?");
+// /* */
+// var loginName = prompt("Who are you?");
 
 
-if (loginName === "hector") {
-    alert("Welcome to your first plugin!");
-} else {
-    var answer = prompt("Are you a friend of Hector?");
-    if (answer === "yes") {
-        alert("Ok you can enter my sample plugin");
-    } else {
-        alert("Ooopppssss!!!!! Impakto ka!");
-    }
-}
+// if (loginName === "hector") {
+//     alert("Welcome to your first plugin!");
+// } else {
+//     var answer = prompt("Are you a friend of Hector?");
+//     if (answer === "yes") {
+//         alert("Ok you can enter my sample plugin");
+//     } else {
+//         alert("Ooopppssss!!!!! Impakto ka!");
+//     }
+// }
 
+var images = [
+                'images/front-view-large.jpg',
+                'images/left-view-large.jpg',
+                'images/rear-view-large.jpg',
+                'images/right-view-large.jpg'
+            ];
+
+for (var images = 0; images <= 4; images++) {
+    console.log(images[2])
+};
